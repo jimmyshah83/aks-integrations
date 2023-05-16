8245e157aec01f4b7c90b27dccf4c35efae33125
Jimmy Shah <jimmyshah83@gmail.com>
2023-05-16 13:53:50 -0400

    Refactor tf files and add working directory for all jobs

---
+++ terraform/providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+
+  # Needs to be pre-configured in Azure either manually or via another Terraform script
+  backend "azurerm" {
+    resource_group_name  = "rg-tf-github-actions-state"
+    storage_account_name = "tfgithubactions01"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
