ab988400219414da5d9538affdf2d5df761611c0
Jimmy Shah <jimmyshah83@gmail.com>
2023-06-15 13:50:56 -0400

    Spring Kafka integration

---
+++ spring-cloud-stream-kafka/Readme.md
@@ -0,0 +1,4 @@
+#### Helpful links
+
+1. https://learn.microsoft.com/en-us/azure/developer/java/spring-framework/kafka-support?tabs=SpringCloudAzure4x  
+2. https://learn.microsoft.com/en-us/azure/developer/java/sdk/identity-azure-hosted-auth  
---
+++ spring-cloud-stream-kafka/src/main/java/com/example/kafka/SpringCloudStreamKafkaApplication.java
@@ -0,0 +1,39 @@
+package com.example.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@SpringBootApplication
+@Slf4j
+public class SpringCloudStreamKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamKafkaApplication.class, args);
+	}
+
+	@Bean
+	public Sinks.Many<Message<String>> many() {
+		return Sinks.many().unicast().onBackpressureBuffer();
+	}
+
+	@Bean
+	public Supplier<Flux<Message<String>>> supply(Sinks.Many<Message<String>> many) {
+		return () -> many.asFlux()
+				.doOnNext(m -> log.info("Manually sending message {}", m))
+				.doOnError(t -> log.error("Error encountered", t));
+	}
+
+	@Bean
+	public Consumer<Message<String>> consume() {
+		return message -> log.info("New message received: '{}'", message.getPayload());
+	}
+
+}
